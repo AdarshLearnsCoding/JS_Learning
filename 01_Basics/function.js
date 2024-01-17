9e26f5705e26f34389138624b2cd9f8d91072ebf
AdarshLearnsCoding <156497872+AdarshLearnsCoding@users.noreply.github.com>
2024-01-17 04:45:08 +0000

    function expression

--- 01_Basics/function.js
+++ 01_Basics/function.js
@@ -1,5 +1,10 @@
-function addValue(){
-    return(
-        counter = counter + 1
-    )
+function addValue(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        console.log( a + b)
+    } else {
+        console.log("incorrect parameter data types")
+    }
 }
+
+addValue(2, 3)
+
